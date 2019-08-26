7183f4010a1f5feead09dfcf43c2e7fac8b3be07
mlashner <lashnem@gmail.com>
2019-08-25 21:48:35 -0400

    Remove commented code

--- src/services/FavoriteService.js
+++ src/services/FavoriteService.js
@@ -1,14 +1,7 @@
 export default class FavoriteService {
     static createFavorite(userId, workshopId) {
         return fetch(`http://127.0.0.1:8080/favorites?userId=${encodeURIComponent(userId)}&workshopId=${encodeURIComponent(workshopId)}`, {
-            method: 'POST',
-            // headers: {
-            //   Accept: 'application/json',
-            //   'Content-Type': 'application/json',
-            // },
-            // body: JSON.stringify({
-            //   favorite
-            // }),
+            method: 'POST'
           })
           .then((favorite) => {return favorite.json()})
           .catch((error) => console.log('this is an error' + error));
