ade126657ed2f388532c4e9165bd4a026d0eea1f
Anjali Justice <anjalijustice@MacBook-Pro.localdomain>
2020-02-01 14:45:15 -0500

    utils and event page formatting updated

--- src/components/ScheduleCard.js
+++ src/components/ScheduleCard.js
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
   cardText: {
     paddingBottom: 10,
     fontSize: 16,
-    margin: 10,
+    margin: 50,
     marginBottom: 0,
     opacity: 1,
     textAlign: 'center',
